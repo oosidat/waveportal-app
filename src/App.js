6bfdf5b0d49d2d21427de73f54acc356150feee3
Osama Sidat <oosidat@gmail.com>
2021-10-26 01:44:23 +0000

    Setup connectWallet and isWalletConnected methods to interact with the wallet/account

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have Metamask!");
+        return;
+      } else {
+        console.log("Ethereum object:", ethereum);
+      }
+      
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.error(error);
+    }   
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected account:", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   const wave = () => {
-    
+    console.log("wave");
   }
+
+  useEffect(() => {
+    isWalletConnected()
+  }, [])
   
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        <span role="img" aria-label="person waving">🙋‍♂️</span> Hi there!
         </div>
-
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          <p>I'm Osama! I work on making flashsales better for Shopify merchants. In my spare time, I enjoy cooking, photography, and learning about Ethereum!</p>
+          <p>Connect your wallet and say hi!</p>
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          <span role="img" aria-label="hand-waving">👋</span> Holler at me!
         </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>Connect Wallet</button>
+        )}
       </div>
     </div>
   );
